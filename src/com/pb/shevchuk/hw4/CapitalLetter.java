e2932d16d53b4701c16cb7d6ad63c6016fd9c2d9
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2021-10-23 23:39:10 +0300

    CapitalLetter done

---
+++ src/com/pb/shevchuk/hw4/CapitalLetter.java
@@ -0,0 +1,26 @@
+package com.pb.shevchuk.hw4;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CapitalLetter {
+    public static void main(String[] args) throws IOException {
+        String line;
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            line = reader.readLine();
+        }
+
+        String[] words = line.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = StringUtils.capitalize(words[i]);
+        }
+
+        String capitalizedLine = String.join(" ", words);
+        System.out.println(capitalizedLine);
+    }
+}
