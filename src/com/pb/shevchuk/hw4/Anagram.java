b9f038536befce077d380d4f9862d07c57ebe73b
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2021-10-28 13:25:30 +0300

    change logic

--- src/com/pb/shevchuk/hw4/Anagram.java
+++ src/com/pb/shevchuk/hw4/Anagram.java
@@ -3,46 +3,52 @@ package com.pb.shevchuk.hw4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Anagram {
     public static void main(String[] args) throws IOException {
-        String[] lines = new String[2];
+        String line1;
+        String line2;
 
         System.out.println("Введіть, будь ласка, по черзі два текстових рядки\n");
 
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            lines[0] = reader.readLine();
-            lines[1] = reader.readLine();
+            line1 = reader.readLine();
+            line2 = reader.readLine();
         }
 
         System.out.println();
 
-        char[][] arrays = new char[2][];
-
-        for (int i = 0; i < lines.length; i++) {
-            arrays[i] = getCharArray(lines[i]);
-        }
-
-        if (arrays[0].length != arrays[1].length) {
-            System.out.println("Рядки не є анаграмами");
-            return;
-        }
-
-        for (char[] array : arrays) {
-            Arrays.sort(array);
-        }
-
-        if (Arrays.equals(arrays[0], arrays[1])) {
+        if (isAnagram(line1, line2)) {
             System.out.println("Рядки є анаграмами");
         } else {
             System.out.println("Рядки не є анаграмами");
         }
     }
 
-    static char[] getCharArray(String text) {
-        text = text.replaceAll("[^a-zA-Zа-яА-Я]", "");
-        text = text.toLowerCase();
-        return text.toCharArray();
+    static boolean isAnagram(String text1, String text2) {
+        String[] texts = {text1, text2};
+
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = texts[i].replaceAll("[^a-zA-Zа-яА-Я]", "");
+            texts[i] = texts[i].toLowerCase();
+        }
+
+        if (texts[0].length() != texts[1].length()) {
+            return false;
+        }
+
+        StringBuilder letters = new StringBuilder(texts[0]);
+
+        for (int i = 0; i < texts[1].length(); i++) {
+            String letter = texts[1].substring(i, i + 1);
+
+            if (letters.indexOf(letter) != -1) {
+                letters.insert(i, "");
+            } else {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
