d616ff18e3ac00ceff574a513a3345ceb4b0b08a
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2021-11-13 22:15:37 +0200

    Clothes class done

--- src/com/pb/shevchuk/hw7/Clothes.java
+++ src/com/pb/shevchuk/hw7/Clothes.java
@@ -1,15 +1,61 @@
 package com.pb.shevchuk.hw7;
 
 public abstract class Clothes {
-    public enum Size {
-        XXS("Дитячий розмір", 32),
-        XS("Дорослий розмір", 34),
-        S("Дорослий розмір", 36),
-        M("Дорослий розмір", 38),
-        L("Дорослий розмір", 40);
+    private final String title;
+    private Size size;
+    private double price;
+    private String color;
 
-        private String description;
-        private int euroSize;
+    public Clothes(String title, Size size, double price, String color) {
+        this.title = title;
+        this.size = size;
+        this.price = price;
+        this.color = color;
+    }
+
+    protected void dress() {
+        System.out.printf(
+                "%s, %s, %s, ціна %.2f грн.\n",
+                title,
+                color,
+                size,
+                price
+        );
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public interface ManClothes {
+        void dressMan();
+    }
+
+    public interface WomanClothes {
+        void dressWoman();
+    }
+
+    public enum Size {
+        XXS("дитячий", 32),
+        XS("дорослий", 34),
+        S("дорослий", 36),
+        M("дорослий", 38),
+        L("дорослий", 40);
+
+        private final String description;
+        private final int euroSize;
 
         Size(String description, int euroSize) {
             this.description = description;
@@ -22,6 +68,11 @@ public abstract class Clothes {
 
         public int getEuroSize() {
             return euroSize;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("розмір %d - %s", euroSize, description);
         }
     }
 }
