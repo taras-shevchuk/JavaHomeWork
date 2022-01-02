99c32a8ec6626db400f443c904e0d3c075a1354d
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2022-01-02 03:03:44 +0200

    hw14 make multithreading

--- src/com/pb/shevchuk/hw14/Client.java
+++ src/com/pb/shevchuk/hw14/Client.java
@@ -2,30 +2,49 @@ package com.pb.shevchuk.hw14;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        int port = 25000;
+    private static final int port = 25000;
+    private final String name;
 
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        new Client((args.length == 0) ? "taras" : args[0]).write();
+    }
+
+    public void write() {
         try (
-            Socket socket = new Socket("localhost", port);
-            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+                Socket socket = new Socket("localhost", port);
+                BufferedReader console = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+                BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                PrintWriter writer = new PrintWriter(
+                        new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
+                        true
+                );
         ) {
-            Thread print = new Thread(new Print(server));
+            Thread print = new Thread(() -> {
+                String message;
+                try {
+                    while ((message = server.readLine()) != null) {
+                        System.out.println("\n " + message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
             print.start();
 
-            System.out.println("client");
+            System.out.println(name + " приєднався до чату");
             String message;
-
             while ((message = console.readLine()) != null) {
-                writer.println(message);
-                System.out.println("send");
+                writer.println(name + ": " + message);
             }
-
-            System.out.println("end client");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/com/pb/shevchuk/hw14/Server.java
+++ src/com/pb/shevchuk/hw14/Server.java
@@ -3,7 +3,8 @@ package com.pb.shevchuk.hw14;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.time.LocalDate;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,22 +12,37 @@ public class Server {
     public static void main(String[] args) throws IOException {
         int port = 25000;
         ServerSocket serverSocket = new ServerSocket(port);
+        Socket socket = serverSocket.accept();
+        Socket socket1 = serverSocket.accept();
 
-        try (
-            Socket socket = serverSocket.accept();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-        ) {
-            System.out.println("server");
-            String message;
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        threadPool.submit(new Handler(socket));
+        threadPool.submit(new Handler(socket1));
+    }
 
-            while ((message = reader.readLine()) != null) {
-                writer.println((LocalDate.now() + ", " + message));
-                System.out.println("get");
+    static class Handler implements Runnable {
+        private final Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+                    PrintWriter writer = new PrintWriter(
+                            new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
+                            true
+                    );
+            ) {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    writer.println(LocalDateTime.now() + "  -----  " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            System.out.println("end server");
         }
     }
 }
