9fde2b1f9182a3eed6e415b984008925b4d95177
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2022-01-04 19:49:51 +0200

    hw14 improve done

--- src/com/pb/shevchuk/hw14/Client.java
+++ src/com/pb/shevchuk/hw14/Client.java
@@ -6,35 +6,50 @@ import java.nio.charset.StandardCharsets;
 
 public class Client {
     private static final int port = 25000;
+    private final String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
-        try (
-                Socket socket = new Socket("localhost", port);
-                BufferedReader console = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-                BufferedReader server = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8)
-                );
-                PrintWriter writer = new PrintWriter(
-                        new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
-                        true
-                )
-        ) {
-            Thread print = new Thread(() -> {
-                String message;
-                try {
-                    while ((message = server.readLine()) != null) {
-                        System.out.println(message);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            print.start();
-            System.out.println("client start\n");
+        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
+            System.out.println("Вітаємо у чаті, введіть ваше ім'я");
+            new Client(console.readLine()).chat(console);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chat(BufferedReader reader) {
+        try (
+            Socket socket = new Socket("localhost", port);
+            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            PrintWriter writer = new PrintWriter(
+                new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
+                true
+            )
+        ) {
+            writer.println(name);
+            new Thread(() -> print(server)).start();
+            System.out.println(name + " у чаті");
+
+            System.out.println("\nрозпочніть спілкування");
             String message;
-            while ((message = console.readLine()) != null) {
+            while ((message = reader.readLine()) != null) {
                 writer.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void print(BufferedReader reader) {
+        String message;
+        try {
+            while ((message = reader.readLine()) != null) {
+                System.out.println(message);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/com/pb/shevchuk/hw14/Server.java
+++ src/com/pb/shevchuk/hw14/Server.java
@@ -5,6 +5,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,39 +16,58 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("server start");
         ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        threadPool.submit(new Sender());
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            threadPool.submit(new Handler(clientSocket));
+            Receiver receiver = new Receiver(clientSocket);
+            Sender.receivers.add(receiver);
+            threadPool.submit(receiver);
         }
     }
 
-    static class Handler implements Runnable {
-        private final Socket socket;
+    static class Receiver implements Runnable {
+        static private final ConcurrentLinkedQueue<String> messages = new ConcurrentLinkedQueue<>();
 
-        public Handler(Socket socket) {
-            this.socket = socket;
+        private final String clientName;
+        private final BufferedReader reader;
+        private final PrintWriter writer;
+
+        public Receiver(Socket socket) throws IOException {
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8)
+            );
+            clientName = reader.readLine();
+            writer = new PrintWriter(
+                    new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
+                    true
+            );
         }
 
         @Override
         public void run() {
-            try (
-                    BufferedReader reader = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8)
-                    );
-                    PrintWriter writer = new PrintWriter(
-                            new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
-                            true
-                    )
-            ) {
+            try {
                 String message;
                 while ((message = reader.readLine()) != null) {
-                    writer.println(LocalDateTime.now() + "  -----  " + message);
+                    messages.add(String.format("%s  ----  %s: %s", LocalDateTime.now(), clientName, message));
                 }
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    static class Sender implements Runnable {
+        private static final List<Receiver> receivers = new ArrayList<>();
+
+        @Override
+        public void run() {
+            while (true) {
+                if (!Receiver.messages.isEmpty()) {
+                    String message = Receiver.messages.remove();
+                    receivers.forEach(receiver -> receiver.writer.println(message));
+                }
             }
         }
     }
