8c146456984850a68f25b722adbfa019a9f55954
Taras Shevchuk <taras.shevchuk@privatbank.ua>
2022-01-09 00:56:43 +0200

    hw14 add GUI

--- src/com/pb/shevchuk/hw14/Client.java
+++ src/com/pb/shevchuk/hw14/Client.java
@@ -1,58 +1,87 @@
 package com.pb.shevchuk.hw14;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
-public class Client {
-    private static final int port = 25000;
-    private final String name;
+public class Client extends JFrame implements ActionListener {
+    private static final int PORT = Server.PORT;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
 
-    public Client(String name) {
-        this.name = name;
+    public static void main(String[] args) throws IOException {
+        SwingUtilities.invokeLater(Client::new);
     }
 
-    public static void main(String[] args) {
-        try (BufferedReader console = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
-            System.out.println("Вітаємо у чаті, введіть ваше ім'я");
-            new Client(console.readLine()).chat(console);
+    private String name;
+    BufferedReader server;
+    PrintWriter writer;
+    private final JTextArea log = new JTextArea();
+    private final JTextField input = new JTextField();
+
+    private Client() {
+        try {
+            Socket socket = new Socket("localhost", PORT);
+            server = new BufferedReader(new InputStreamReader(socket.getInputStream(), Server.UTF8));
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), Server.UTF8), true);
 
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        log.setText("введіть ваше ім'я\n");
+        add(log, BorderLayout.CENTER);
+
+        input.addActionListener(this);
+        add(input, BorderLayout.SOUTH);
+
+        setVisible(true);
+        new Thread(this::listenMessage).start();
     }
 
-    public void chat(BufferedReader reader) {
-        try (
-            Socket socket = new Socket("localhost", port);
-            BufferedReader server = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-            PrintWriter writer = new PrintWriter(
-                new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8),
-                true
-            )
-        ) {
-            writer.println(name);
-            new Thread(() -> print(server)).start();
-            System.out.println(name + " у чаті");
-
-            System.out.println("\nрозпочніть спілкування");
-            String message;
-            while ((message = reader.readLine()) != null) {
-                writer.println(message);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void print(BufferedReader reader) {
+    private void listenMessage() {
         String message;
         try {
-            while ((message = reader.readLine()) != null) {
-                System.out.println(message);
+            while ((message = server.readLine()) != null) {
+                print(message);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void print(String message) {
+        SwingUtilities.invokeLater(() -> {
+            log.append(message + "\n");
+            log.setCaretPosition(log.getDocument().getLength());
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String message = input.getText();
+
+        if (name == null) {
+            name = (message.length() != 0) ? message : "anonim";
+            setTitle(name);
+            message = name;
+            log.setText("введіть повідомлення\n");
+        } else {
+            message = input.getText();
+            if (message.equals("")) return;
+        }
+        writer.println(message);
+        input.setText("");
     }
 }
